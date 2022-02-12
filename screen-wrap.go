ce4b67e8379735d6982626e3d809deb3fb97f084
William Anthony <william_anthony@hotmail.co.uk>
2022-02-12 21:42:01 +0000

    Component to destroy objects upon leaving screen.

--- screen-wrap.go
+++ screen-wrap.go
@@ -4,12 +4,24 @@ type screenWrap struct {
 	parent *entity
 }
 
+type screenKill struct {
+	parent *entity
+}
+
 func createScreenWrap(parent *entity) *screenWrap {
 	sw := screenWrap{}
 
 	sw.parent = parent
 
 	return &sw
+}
+
+func createScreenKill(parent *entity) *screenKill {
+	sk := screenKill{}
+
+	sk.parent = parent
+
+	return &sk
 }
 
 func (sw *screenWrap) update() error {
@@ -32,5 +44,28 @@ func (sw *screenWrap) update() error {
 }
 
 func (sw *screenWrap) draw() error {
+	return nil
+}
+
+func (sw *screenKill) update() error {
+	if sw.parent.position.X > screenWidth {
+		sw.parent.active = false
+	}
+
+	if sw.parent.position.X < 0 {
+		sw.parent.active = false
+	}
+
+	if sw.parent.position.Y > screenHeight {
+		sw.parent.active = false
+	}
+
+	if sw.parent.position.Y < 0 {
+		sw.parent.active = false
+	}
+	return nil
+}
+
+func (sw *screenKill) draw() error {
 	return nil
 }
