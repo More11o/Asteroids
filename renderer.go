bf01ff6eaad70e6cdb39bfdd6f44085bc571703e
William Anthony <william_anthony@hotmail.co.uk>
2022-02-12 00:41:50 +0000

    Division not required. Added note for future fix.

--- renderer.go
+++ renderer.go
@@ -23,10 +23,10 @@ func createRenderer(parent *entity, filename string) *renderer {
 func (r *renderer) draw() error {
 	position := r.parent.position
 
-	// Convert center to top left.
-	position.X -= float32(r.texture.Width) / 2.0
-	position.Y -= float32(r.texture.Height) / 2.0
+	position.X -= float32(r.texture.Width)
+	position.Y -= float32(r.texture.Height)
 
+	// FIXME: Should rotate around center point
 	rl.DrawTextureEx(r.texture, position, r.parent.rotation, r.parent.scale, rl.White)
 
 	return nil
